5ccd972696f14d837053561ed8a00e3e12ffc0fe
aysinsimsek25 <aysinsimsek61@gmail.com>
2026-08-21 19:35:25 +0300

    Add HW.cs with string manipulation examples

---
+++ HW.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var ifade = " Merhaba programlama dünyası. ";
+        Console.WriteLine(ifade);
+        Console.WriteLine(ifade.Length);
+        Console.WriteLine(ifade.ToUpper());
+        Console.WriteLine(ifade.ToLower());
+        Console.WriteLine(ifade.TrimStart());
+        Console.WriteLine(ifade.TrimEnd());
+        Console.WriteLine(ifade[0]);
+        Console.WriteLine(ifade[1]);
+        Console.WriteLine(ifade[ifade.Length-1]);
+
+
+
+
+        Console.ReadKey();  
+    }
+}
